7d510e3436409b109ef153da7f29940bbf8b7ceb
刘峰 <zgjllf@msn.cn>
2018-05-11 00:09:38 +0800

    no message

--- Assets/Code/LowPolyTool.cs
+++ Assets/Code/LowPolyTool.cs
@@ -18,22 +18,32 @@ public class LowPolyTool : MonoBehaviour {
     }
 
     static void LowPoly(Transform t){
+
         MeshFilter meshFilter = t.GetComponent<MeshFilter>();  
         Mesh mesh = meshFilter.sharedMesh;  
 
         Vector3[] oldVerts = mesh.vertices;//保存当前Mesh顶点  
         int[] triangles = mesh.triangles;//三角索引数组  
+        Vector2[] olduvs = mesh.uv;
+        BoneWeight[] oldBoneWeights = mesh.boneWeights;
 
-        Vector3[] verts = new Vector3[triangles.Length];//用于保存新的顶点信息  
+        Vector3[] verts = new Vector3[triangles.Length];//用于保存新的顶点信息 
+        Vector2[] uvs = new Vector2[triangles.Length];//用于保存新的顶点信息 
+        BoneWeight[] boneWeights = new BoneWeight[triangles.Length];//用于保存新的顶点信息 
 
         for (int i = 0; i < triangles.Length; i++)
         {
             verts[i] = oldVerts[triangles[i]];
+            uvs[i] = olduvs[triangles[i]];
+            boneWeights[i] = oldBoneWeights[triangles[i]];
             triangles[i] = i;
         }
 
+
         mesh.vertices = verts;//更新Mesh顶点  
-        mesh.triangles = triangles;//更新索引  
+        mesh.triangles = triangles;//更新索引 
+        mesh.uv = uvs;
+        mesh.boneWeights = boneWeights;
         mesh.RecalculateBounds();//重新计算边界  
         mesh.RecalculateNormals();//重新计算法线  
     }
